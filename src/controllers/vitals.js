f67b52f2cac8f6b79f52037f0f08263a5aed9f60
David Castaneda <dcast188@fiu.edu>
2018-11-10 19:51:14 -0500

    added sockets (#2)

--- src/controllers/vitals.js
+++ src/controllers/vitals.js
@@ -1,3 +1,27 @@
+import vitalService from "../services/vitals";
+
+const { FITBIT_CALLBACK_URI } = process.env;
+
+const auth = async (req, res) => {
+  try {
+    const auth_url = await vitalService.authFitbit();
+    res.redirect(auth_url);
+  } catch (e) {
+    res.send(e);
+  }
+};
+
+// const read = async (req, res) => {
+//   const { code } = req.query;
+
+//   try {
+//     const vitals = await vitalService.readFitbit(code);
+//     res.send({ vitals });
+//   } catch (e) {
+//     res.send(e);
+//   }
+// };
+
 const read = async (req, res) => {
   try {
     res.send(true);
@@ -6,4 +30,12 @@ const read = async (req, res) => {
   }
 };
 
-export default { read };
+const create = async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (e) {
+    res.send(e);
+  }
+};
+
+export default { auth, read, create };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
 import vitals from "../controllers/vitals";
+import vital from "../controllers/vital";
 import courier from "../controllers/courier";
 
 const apiRouter = Router();
 
 apiRouter.get("/vitals", vitals.read);
+apiRouter.post("/vitals", vitals.create);
+
+apiRouter.get("/vital", vital.read);
+apiRouter.post("/vital", vital.create);
 
 apiRouter.post("/text", courier.text);
 
